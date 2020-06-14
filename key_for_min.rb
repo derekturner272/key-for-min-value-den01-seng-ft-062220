e59349b23576fbe9a30728a0c9588b33b01b0c55
Derek Turner <turner.derek.b@gmail.com>
2020-06-14 00:01:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-    if name_hash.length == 0 
+  min_value = 999999999
+  if name_hash.length == 0 
     puts nil 
   else 
-  min_value = 999999999
-  name_hash.each do |key, value|
-    if value <= min_value
-      min_value = value
-    end
+    name_hash.each do |key, value|
+      if value <= min_value
+        min_value = value
+      end
     
     end
   end
